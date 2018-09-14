47d2817cd72792f9fce9c9203f538a3a9f96997d
nw2190 <nwinovic@purdue.edu>
2018-09-14 18:28:35 -0400

    Add paper LaTeX files

---
+++ SVJour3/code/l2_dist.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+c1 = [[0.001139, -0.003129], [0.000692, -0.003257], [0.000232, -0.003322]]
+
+c2 = [[-0.010100, 0.002305], [-0.009987, 0.002756], [-0.009853, 0.003201]]
+
+
+print(c1[0])
+
+def l2_dist(x,y):
+    return np.sqrt(np.power(x[0]-y[0],2)+np.power(x[1]-y[1],2))
+
+d1 = []
+for i in range(0,3):
+    for j in range(0,3):
+        d1.append(l2_dist(c1[i],c1[j]))
+d1_max = np.max(d1)
+
+d2 = []
+for i in range(0,3):
+    for j in range(0,3):
+        d2.append(l2_dist(c2[i],c2[j]))
+d2_max = np.max(d2)
+
+print(d1_max)
+print(d2_max)
